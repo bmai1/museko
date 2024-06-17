b1af19ce352e7322126e355922cde6ab3d677aeb
Brian Mai <0hrs3g@gmail.com>
2024-06-16 20:37:13 -0700

    Move model files

---
+++ museko/essentia_model/genre_discogs400.py
@@ -0,0 +1,75 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import io 
+import base64
+
+
+from essentia.standard import MonoLoader, TensorflowPredictEffnetDiscogs, TensorflowPredict2D
+from .genre_data import genre_names
+
+def classify(filename):
+    # Absolute paths for models
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    effnet_model_path = os.path.join(base_dir, "discogs-effnet-bs64-1.pb")
+    genre_model_path = os.path.join(base_dir, "genre_discogs400-discogs-effnet-1.pb")
+
+    if not os.path.exists(effnet_model_path):
+        raise FileNotFoundError(f"Effnet model file not found at {effnet_model_path}")
+    if not os.path.exists(genre_model_path):
+        raise FileNotFoundError(f"Genre model file not found at {genre_model_path}")
+    
+    audio = MonoLoader(filename=filename, sampleRate=16000, resampleQuality=4)()
+
+    # Extract embeddings
+    embedding_model = TensorflowPredictEffnetDiscogs(
+        graphFilename=effnet_model_path,
+        output="PartitionedCall:1"
+    )
+    embeddings = embedding_model(audio)
+
+    # Predict with genre classification model
+    model = TensorflowPredict2D(
+        graphFilename=genre_model_path,
+        input="serving_default_model_Placeholder", 
+        output="PartitionedCall:0"
+    )
+    predictions = model(embeddings)
+    average_predictions = np.mean(predictions, axis=0)
+
+    # Sort genres by average prediction scores
+    sorted_indices = np.argsort(average_predictions)[::-1]  # Descending order
+    sorted_genres = [genre_names[i] for i in sorted_indices]
+    sorted_predictions = average_predictions[sorted_indices]
+
+    # Select top N genres to display
+    top_n = 10
+    top_genres = sorted_genres[:top_n]
+    top_scores = sorted_predictions[:top_n]
+
+    
+    # Plot the results
+
+    # IMPORTANT: switch to Agg to prevent crashing on MacOS, so server doesn't create and destroy GUI windows
+    # UserWarning: Starting a Matplotlib GUI outside of the main thread will likely fail.
+    # NSInternalInconsistencyException', reason: 'NSWindow drag regions should only be invalidated on the Main Thread!
+
+    plt.switch_backend('Agg') 
+
+    plt.figure(figsize=(12, 6))
+    plt.bar(top_genres, top_scores, color='skyblue')
+    plt.xlabel('Genres')
+    plt.ylabel('Average Prediction Scores')
+    plt.title(f'Top {top_n} Genres by Average Prediction')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+
+    # plt.show()
+
+    # save plot image to serve with flask
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+
+    return buffer
