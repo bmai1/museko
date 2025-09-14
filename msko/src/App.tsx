3f8f3f56f86a6b92fa38521d2e23d8103478937a
Brian Mai <0hrs3g@gmail.com>
2025-09-13 19:01:28 -0700

    Remove library for now

--- msko/src/App.tsx
+++ msko/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Upload from "./pages/Upload";
 import Download from "./pages/Download";
-import Library from "./pages/Library";
+// import Library from "./pages/Library";
 import Explore from "./pages/Explore";
 
 import "./App.css";
@@ -16,7 +16,7 @@ function App() {
       <main className="p-6">
         {page === "upload" && <Upload />}
         {page === "download" && <Download />}
-        {page === "library" && <Library />}
+        {/* {page === "library" && <Library />} */}
         {page === "explore" && <Explore />}
       </main>
     </div>
