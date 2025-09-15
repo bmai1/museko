6889c43dc08fbe3ec9d768f2f8d2c8a89c21af26
Brian Mai <0hrs3g@gmail.com>
2025-09-15 13:56:21 -0700

    Update upload component

--- msko/src/pages/Upload.tsx
+++ msko/src/pages/Upload.tsx
@@ -1,10 +1,9 @@
-import FileUpload from "../components/FileUpload";
+import Classifier from "../components/Classifier";
 
 export default function Upload() {
   return (
     <div className="mt-8 flex flex-col items-center">
-      <h2 className="mb-4 text-xl">Genre Classification</h2>
-      <FileUpload />
+      <Classifier />
     </div>
   );
 }
