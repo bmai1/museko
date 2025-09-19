861d1fd7fb3f97788efea64abed5995558a81aa9
Brian Mai <0hrs3g@gmail.com>
2025-09-18 20:52:16 -0700

    Dynamic sizing for upload div

--- msko/src/App.tsx
+++ msko/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Upload from "./pages/Upload";
 import Download from "./pages/Download";
-// import Library from "./pages/Library";
+import Library from "./pages/Library";
 import Explore from "./pages/Explore";
 
 import "./App.css";
@@ -11,12 +11,12 @@ function App() {
   const [page, setPage] = useState("upload");
 
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen text-white bg-black">
       <Navbar page={page} setPage={setPage} />
       <main className="p-6">
         {page === "upload" && <Upload />}
         {page === "download" && <Download />}
-        {/* {page === "library" && <Library />} */}
+        {page === "library" && <Library />}
         {page === "explore" && <Explore />}
       </main>
     </div>
--- msko/src/components/Classifier.tsx
+++ msko/src/components/Classifier.tsx
@@ -26,13 +26,12 @@ function FileUpload({ onFileProcessed }) {
   return (
     <div
       {...getRootProps()}
-      className={`file-upload ${isDragActive ? "bg-gray-500 border-white" : ""} rounded-xl border border-gray-500 p-12 text-center`}
+      className={`w-[clamp(200px,50vw,800px)] h-[clamp(200px,50vh,800px)] file-upload ${isDragActive ? "bg-gray-500 border-white" : ""} rounded-xl border border-gray-500 p-12 text-center`}
     >
-      <h2 className="mb-4 text-xl">Genre Classification</h2>
       <input {...getInputProps()} />
-      <p>Drag and drop audio file here</p>
-      <p className="text-sm text-gray-400">or</p>
-      <button className="cursor-pointer rounded-lg bg-gray-700 px-4 py-2 text-gray-500">Browse your files</button>
+      <p className="mt-[3vh] text-[clamp(1rem,2vw,2.5rem)]">Drag and drop audio file here</p>
+      <p className="text-[clamp(1rem,2vw,2.5rem)] mb-[1vh] text-gray-400">or</p>
+      <button className="cursor-pointer rounded-lg bg-gray-700 px-4 py-2 text-[clamp(1rem,1.5vw,2rem)] text-gray-500">Browse your files</button>
     </div>
     
   );
@@ -52,7 +51,7 @@ function GenrePlot({ filename }) {
     
   return (
     <div>
-      <h2 className="mb-2 text-lg font-bold">Genre Plot for {filename}</h2>
+      <h2 className="mb-2 truncate w-100">Predicted genres for {filename}</h2>
       {plotPath ? (
         <img
           src={plotPath}
@@ -69,7 +68,7 @@ function GenrePlot({ filename }) {
 export default function Classifier() {
   const [processedFile, setProcessedFile] = useState<string | null>(null);
   return (
-    <div>
+    <div className="">
       {processedFile ? (
         <GenrePlot filename={processedFile} />
       ) : (
