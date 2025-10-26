33b1cb12acf61ad3912dd24d2b43b7fba931d652
Brian Mai <0hrs3g@gmail.com>
2025-10-26 15:33:33 -0700

    test

--- msko/src/App.tsx
+++ msko/src/App.tsx
@@ -12,6 +12,11 @@ function App() {
 
   return (
     <div className="min-h-screen text-white bg-black">
+      <div id="museko">
+        <p>This is</p>
+        <h1>MUSEKO</h1>
+        <button>Let's get started</button>
+      </div>
       <Navbar page={page} setPage={setPage} />
       <main className="p-6">
         {page === "upload" && <Upload />}
