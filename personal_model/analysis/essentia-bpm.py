3f2747267f7de3c012c16e48ca56b4b15a34332b
Brian Mai <0hrs3g@gmail.com>
2025-10-31 15:21:17 -0700

    Remove testing mp3

--- personal_model/analysis/essentia-bpm.py
+++
@@ -1,8 +0,0 @@
-import essentia.standard as es
-
-audio = es.MonoLoader(filename='filename.mp3')()
-
-rhythm_extractor = es.RhythmExtractor2013(method="multifeature")
-bpm, beats, beats_confidence, _, beats_intervals = rhythm_extractor(audio)
-
-print(bpm)
--- personal_model/analysis/librosa-bpm.py
+++
@@ -1,6 +0,0 @@
-import librosa 
-
-y, sr = librosa.load('filename.mp3', duration=60)
-tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
-
-print(tempo)
